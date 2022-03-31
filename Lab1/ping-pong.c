06f5155c6ff4957bee1a7bff61b10ec10066bfd8
lucasbilo <lbilo@fi.uba.ar>
2022-03-30 21:21:46 -0300

    Ping pong terminado

--- Lab1/ping-pong.c
+++ Lab1/ping-pong.c
@@ -5,8 +5,7 @@
 int main(int argc, char* argv[]) {
         int fds_h[2];
 	int fds_p[2];
-        int msg_p = random();
-	int msg_h = random();
+        int msg= random();
         pipe(fds_p);
 	pipe(fds_h);
         int i = fork();
@@ -21,16 +20,14 @@ int main(int argc, char* argv[]) {
         	// El hijo va a escribir en fds_h y leer en fds_p
 		close(fds_p[1]);
                 close(fds_h[0]);
+
                 int recv_h = 0;
                 read(fds_p[0], &recv_h, sizeof(recv_h));
-                printf("[hijo] lei: %d\n", recv_h);
+                printf("[hijo] recibi: %d\n", recv_h);
                 close(fds_p[0]);
 		
-		printf("[hijo] envio el mensaje: %d\n", msg_h);
-                write(fds_h[1], &msg_h, sizeof(msg_h));
+                write(fds_h[1], &msg, sizeof(msg));
                 close(fds_h[1]);
-
-		printf("[hijo] termino\n");
 
         } else {
                 printf("[padre] mi pid es: %d\n", getpid());
@@ -38,15 +35,15 @@ int main(int argc, char* argv[]) {
                 close(fds_h[1]);
 		close(fds_p[0]);
         	
-		printf("[padre] envio el mensaje: %d\n", msg_p);
-                write(fds_p[1], &msg_p, sizeof(msg_p));
+                write(fds_p[1], &msg, sizeof(msg));
                 close(fds_p[1]);
 
 		int recv_p = 0;
 		read(fds_h[0], &recv_p, sizeof(recv_p));
-		printf("[padre] lei: %d\n", recv_p);
+		printf("[padre] recibi: %d\n", recv_p);
                 close(fds_h[0]);
 
-		printf("[padre] termino\n");
+                int ret = wait(NULL);
+                printf("Proceso %d termino \n", ret);
        }
 }
